fe8002a627bb124a3412567dc877ebcb7d4c78ba
veroca88 <veronicadonoso1506@gmail.com>
2020-05-31 23:14:16 -0400

    Basics of react-native

--- App.js
+++ App.js
@@ -1,19 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log(require("./assets/icon.png"));
+    // console.log("text press");
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={10} onPress={handlePress}>
+        Hello World!! Contrary to popular belief, Lorem Ipsum is not simply
+        random text. It has roots in a piece of classical Latin literature from
+        45 BC, making it over 2000 years old. Richard McClintock, a Latin
+        professor at Hampden-Sydney College in Virginia, looked up one of the
+        more obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+        going through the cites of the word in classical literature, discovered
+        the undoubtable source.
+      </Text>
+      {/* <Image source={require("./assets/icon.png")} /> */}
+      <TouchableOpacity
+        onPress={() => {
+          console.log("Image Tapped");
+        }}
+      >
+        <Image
+          // blurRadius={10}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+        <Button
+          title="Click me"
+          onPress={() => {
+            alert("Button clicked");
+          }}
+        />
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fda7cb",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
